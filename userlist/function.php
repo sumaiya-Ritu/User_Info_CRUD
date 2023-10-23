c2eeaabfd25cabf1b804716604c06df73b3939ed
sumaiya-Ritu <sumaiyaritu9727@gmail.com>
2023-10-23 13:12:04 -0500

    final push

--- userlist/function.php
+++ userlist/function.php
@@ -20,7 +20,7 @@ function getuserList()
             $data = [
                 'status' => 200,
                 'message' => 'User list fetched successfully',
-                'data'=> $res, //sending data in response
+                'data' => $res, //sending data in response
             ];
             header("http/1.0 200 User list fetched successfully");
             return json_encode($data);
@@ -47,7 +47,8 @@ function getuserList()
 }
 
 //error422 fucntion declaration
-function error422($message){
+function error422($message)
+{
 
     $data = [
         'status' => 422,
@@ -59,56 +60,51 @@ function error422($message){
 }
 
 //declarion the get users for single id call
-function getusers($userparams)  {
+function getusers($userparams)
+{
     global $conn;
 
-    if($userparams['id'] == null){
+    if ($userparams['id'] == null) {
 
         return error422('Enter your user ID');
     }
-$userID = mysqli_real_escape_string($conn,$userparams['id']); //passing the user id parameter in user id function
+    
+    $userID = mysqli_real_escape_string($conn, $userparams['id']); //passing the user id parameter in user id function
 
-$query = "SELECT * FROM users WHERE id = '$userID' LIMIT 1";
-$result = mysqli_query($conn,$query);
+    $query = "SELECT * FROM users WHERE id = '$userID' LIMIT 1";
+    $result = mysqli_query($conn, $query);
 
-if($result){
+    if ($result) {
 
-    //for checking 1 record at a time
-if(mysqli_num_rows($result) == 1){
+        //for checking 1 record at a time
+        if (mysqli_num_rows($result) == 1) {
 
-    $res = mysqli_fetch_assoc($result);
+            $res = mysqli_fetch_assoc($result);
 
-    $data = [
+            $data = [
 
-        'status'=> 200,
-        'message' => 'User Fetched successfully',
-        'data' => $res,
-    ];
-    header("http/1.0 200 User Ok");
-    return json_encode($data);
+                'status' => 200,
+                'message' => 'User Fetched successfully',
+                'data' => $res,
+            ];
+            header("http/1.0 200 User Ok");
+            return json_encode($data);
+        } else {
+            $data = [
+                'status' => 404,
+                'message' => 'No user Found',
+            ];
 
-}else{
-    $data = [
-        'status' => 404,
-        'message' => 'No user Found',
-    ];
+            header("http/1.0 404 No user Found");
+            return json_encode($data);
+        }
+    } else {
+        $data = [
+            'status' => 500,
+            'message' => 'Internal Server Error',
+        ];
 
-    header("http/1.0 404 No user Found");
-    return json_encode($data);
-
-}
-
-
-}else{
-    $data = [
-        'status' => 500,
-        'message' => 'Internal Server Error',
-    ];
-
-    header("http/1.0 500 Internal Server Error");
-    return json_encode($data);
-
-}
-
-
+        header("http/1.0 500 Internal Server Error");
+        return json_encode($data);
+    }
 }
