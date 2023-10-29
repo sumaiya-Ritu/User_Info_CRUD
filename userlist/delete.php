624730b891949fbbdd43fcd1dcb6046cab1bb05c
sumaiya-Ritu <sumaiyaritu9727@gmail.com>
2023-10-29 09:57:49 -0500

    create successfull and initializing delete

---
+++ userlist/delete.php
@@ -0,0 +1,37 @@
+<?php
+
+header('Access-Control-Allow-origin:*'); //allows everything
+header('Content-Type: application/json'); //only json format applicable
+header('Access-Control-Allow-Method: GET'); //only get availalbe
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers,Authorization,X-Request-With'); //allows input
+
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+include('function.php');
+
+if ($requestMethod == "GET") {
+
+    //single id call
+    //isset function clarifies whether the id is set or not
+    
+    if (isset($_GET['id'])) {
+        $users = getusers($_GET);
+        echo $users;
+    } else {
+        
+        $userList = getuserList(); //for showing userlsit
+        echo $userList;
+    }
+
+    // $userList = getuserList(); //for showing userlsit
+    // echo $userList;
+} else {
+
+    $data = [
+        'status' => 405,
+        'message' => $requestMethod . 'Method Not Allowed',
+    ];
+
+    header("http/1.0 405 Method Not Allowed");
+    echo json_encode($data); 
+    //json_encode turns php format to json format
+}
